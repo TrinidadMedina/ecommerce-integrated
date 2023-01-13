2ee180f54c72007be1ed6071a82cfa5e6d6d1548
Trinidad Medina <trinidad.medina.silva@gmail.com>
2023-01-13 17:58:22 -0300

    views-templates

---
+++ public/main.js
@@ -0,0 +1,13 @@
+/* const socket = io();
+
+const formCreateUser = document.getElementById('formCreateUser');
+
+let authorEmail = '';
+
+formCreateAuthor.addEventListener('submit', e => {
+    e.preventDefault()
+    const userName: formCreateUser[0].value;
+    formCreateUser.reset();
+    //SESSION
+});   
+ */
